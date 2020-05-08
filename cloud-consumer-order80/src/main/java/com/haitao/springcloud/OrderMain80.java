c348e909d56057c410613397a46fc75edb2df054
guohaitao <18838951959@163.com>
2020-05-08 19:04:58 +0800

    增加80消费者

---
+++ cloud-consumer-order80/src/main/java/com/haitao/springcloud/OrderMain80.java
@@ -0,0 +1,18 @@
+package com.haitao.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @version 1.0.0
+ * @ClassName OrderMain80
+ * @Description TODO
+ * @Author Tao
+ * @Date 2020/5/7 18:21
+ */
+@SpringBootApplication
+public class OrderMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMain80.class,args);
+    }
+}
---
+++ cloud-consumer-order80/src/main/java/com/haitao/springcloud/controller/OrderController.java
@@ -0,0 +1,14 @@
+package com.haitao.springcloud.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @version 1.0.0
+ * @ClassName OrderController
+ * @Description TODO
+ * @Author Tao
+ * @Date 2020/5/7 18:24
+ */
+@RestController
+public class OrderController {
+}
